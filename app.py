00731adcef86ebaf216c912423b6976ddec77a0f
Joonas Maanonen <joonas.maanonen@aalto.fi>
2019-04-22 20:31:47 +0300

    Made the code more modular and started working with the city bike API

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import json
+import requests
 
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open('config.json', 'r') as f:
+        data = json.load(f)
+
+
+    return render_template('index.html', bike_stations = data['bikeRentalStations'])
 
 if __name__ == '__main__':
     app.run(debug=True)
